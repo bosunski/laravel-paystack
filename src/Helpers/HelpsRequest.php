d12efdd41735d434f6bd45a2524e9eca7fd761d0
Olatunbosun Egberinde <bosunski@gmail.com>
2019-01-02 00:55:01 +0100

    Add Dockblock

--- src/Helpers/HelpsRequest.php
+++ src/Helpers/HelpsRequest.php
@@ -1,8 +1,22 @@
 <?php
 
+/*
+ * This file is part of the Laravel Paystack package.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Unicodeveloper\Paystack\Helpers;
 
 trait HelpsRequest {
+
+	/**
+	 * Helps to populate a request with an array of key value pairs required by the Package
+	 * @param array $args
+	 */
     public function addRequestData(array $args)
     {
         foreach ($args as $key => $data) {
